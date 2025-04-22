18b0618b9c2b9f2bb20ac9b439cc2bf47ab52b8a
ISSA SOW <164485544+thalesow00@users.noreply.github.com>
2025-04-22 01:14:04 +0000

    Add files via upload

---
+++ lib/02_calculator.rb
@@ -0,0 +1,14 @@
+# Fonction qui additionne deux nombres
+def add(a, b)
+  a + b
+end
+
+# Fonction qui soustrait un nombre à un autre
+def subtract(a, b)
+  a - b
+end
+
+# Fonction qui calcule la somme des éléments d'un tableau
+def sum(array)
+  array.sum
+end
