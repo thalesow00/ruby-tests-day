63d5cf7b8b14a3c91b1edd6eb699bdd8a26d41ec
ISSA SOW <164485544+thalesow00@users.noreply.github.com>
2025-04-22 00:56:51 +0000

    Add files via upload

---
+++ 01_temperature.rb
@@ -0,0 +1,9 @@
+# Conversion de Fahrenheit vers Celsius
+def ftoc(fahrenheit)
+  (fahrenheit - 32) * 5.0 / 9.0
+end
+
+# Conversion de Celsius vers Fahrenheit
+def ctof(celsius)
+  (celsius * 9.0 / 5.0) + 32
+end
